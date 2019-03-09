19abcf7592dcd800d3028e3a05e15d0300554403
Rentaro Nomura <n11ren21@icloud.com>
2019-03-09 11:21:03 +0900

    Initial Commit

---
+++ REM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  REM
+//
+//  Created by Rentaro Nomura on 2019/03/09.
+//  Copyright © 2019 Rentaro Nomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
